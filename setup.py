2f63af7ed0443bba312ed64bbae3430bb7e41aba
Jeff Hann <jeffhann@gmail.com>
2013-09-12 23:05:59 -0300

    final tidy up after figuring out brew

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
 	version='0.1.0',
 	author='Jeffrey Hann',
 	author_email='jeffhann@gmail.com',
-	scripts=['bin/cownet.py'],
+	scripts=['bin/cownet'],
 	url='http://obihann.github.io/Cownet/',
 	license='LICENSE',
 	description='I wanted to view my up to date network usage in a easy to read and friendly way. So I built a tool that pulls the data in from netstat, and displays a cleaned up output in Cowsay. ',
